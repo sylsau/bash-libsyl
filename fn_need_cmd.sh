8f813b5dc0b0120f45e30bf95aff0c0679877ac9
Sylvain Saubier (ResponSyS) <mail@systemicresponse.com>
2017-11-27 19:58:12 +0100

    forgot to change fn_err name

--- fn_need_cmd.sh
+++ fn_need_cmd.sh
@@ -2,5 +2,5 @@
 # command (string)
 fn_need_cmd() {
 	command -v "$1" > /dev/null 2>&1
-	[ $? -eq 0 ] ||	fn_err "need '$1' (command not found)" {+ReturnCode+}
+	[ $? -eq 0 ] ||	fn_exit_err "need '$1' (command not found)" {+ReturnCode+}
 }
--- fn_need_file.sh
+++ fn_need_file.sh
@@ -1,5 +1,5 @@
 # Test if a file exists
 # path to file to test (string)
 fn_need_file() {
-	[ -f "$1" ] || fn_err "need '$1' (file not found)" {+ReturnCode+}
+	[ -f "$1" ] || fn_exit_err "need '$1' (file not found)" {+ReturnCode+}
 }
